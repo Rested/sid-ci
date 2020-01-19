3e6989f3127469fd28a356acebb71abd7519238b
Reuben Thomas-Davis <reubenestd@gmail.com>
2020-01-19 23:02:57 +0000

    :poop: Basic sketch of server

--- server/go.mod
+++ server/go.mod
@@ -1,28 +1,13 @@
-module github.com/Rested/sid-ci/server
+module github.com/sid-ci/server
 
-go 1.12
+go 1.13
 
 require (
-	cloud.google.com/go v0.51.0 // indirect
-	cloud.google.com/go/storage v1.5.0 // indirect
-	dmitri.shuralyov.com/gpu/mtl v0.0.0-20191203043605-d42048ed14fd // indirect
-	github.com/creack/pty v1.1.9 // indirect
-	github.com/envoyproxy/go-control-plane v0.9.1 // indirect
-	github.com/go-redis/redis/v7 v7.0.0-beta.5 // indirect
-	github.com/google/go-cmp v0.4.0 // indirect
-	github.com/hashicorp/golang-lru v0.5.3 // indirect
-	github.com/kr/pretty v0.2.0 // indirect
-	github.com/kr/pty v1.1.8 // indirect
-	github.com/prometheus/client_model v0.1.0 // indirect
-	github.com/rogpeppe/go-internal v1.5.1 // indirect
-	github.com/stretchr/objx v0.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20200109152110-61a87790db17 // indirect
-	golang.org/x/image v0.0.0-20191214001246-9130b4cfad52 // indirect
-	golang.org/x/mobile v0.0.0-20191210151939-1a1fef82734d // indirect
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
-	golang.org/x/sys v0.0.0-20200107162124-548cf772de50 // indirect
-	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
-	golang.org/x/tools v0.0.0-20200110004031-563860d11da6 // indirect
-	google.golang.org/genproto v0.0.0-20200108215221-bd8f9a0ef82f // indirect
-	gopkg.in/yaml.v2 v2.2.7 // indirect
+	github.com/Rested/sid-ci v0.0.0-20200115040148-681695053f22
+	github.com/golang/protobuf v1.3.2
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	golang.org/x/sys v0.0.0-20200113162924-86b910548bc1 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/genproto v0.0.0-20200113173426-e1de0a7b01eb // indirect
+	google.golang.org/grpc v1.26.0
 )
--- server/main.go
+++ server/main.go
@@ -1,11 +1,123 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
-	"github.com/Rested/sid-ci/sid"
-	"github.com/go-redis/redis"
+	"io"
+	"log"
+	"net"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/testdata"
+
+	"github.com/golang/protobuf/proto"
+
+	pb "github.com/Rested/sid-ci/sid"
 )
 
-func main(){
-	fmt.Println("hello world")
+var (
+	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile = flag.String("cert_file", "", "The TLS cert file")
+	keyFile  = flag.String("key_file", "", "The TLS key file")
+	port     = flag.Int("port", 10000, "The server port")
+	host     = flag.String("host", "localhost", "The server host")
+)
+
+type sidServer struct {
+	pb.UnimplementedSidServer
+	// pb.UnimplementedRouteGuideServer
+	// savedFeatures []*pb.Feature // read-only after initialized
+
+	mu sync.Mutex // protects routeNotes
+	// routeNotes map[string][]*pb.
+}
+
+// Get
+func (s *sidServer) GetJob(ctx context.Context, healthStatus *pb.HealthStatus) (*pb.Job, error) {
+
+	// No job was found, return an unnamed feature
+	return &pb.Job{Location: point}, nil
+}
+
+// ListFeatures lists all features contained within the given bounding Rectangle.
+func (s *sidServer) HealthStatusCheckIn(healthStatus *pb.HealthStatus, stream pb.Sid_HealthStatusCheckInServer) error {
+	for _, feature := range s.savedFeatures {
+		if inRange(feature.Location, rect) {
+			if err := stream.Send(feature); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// RecordRoute records a route composited of a sequence of points.
+//
+// It gets a stream of points, and responds with statistics about the "trip":
+// number of points,  number of known features visited, total distance traveled, and
+// total time spent.
+func (s *sidServer) RecordJobRun(stream pb.Sid_RecordJobRunServer) error {
+	var pointCount, featureCount, distance int32
+	var lastPoint *pb.Point
+	startTime := time.Now()
+	for {
+		point, err := stream.Recv()
+		if err == io.EOF {
+			endTime := time.Now()
+			return stream.SendAndClose(&pb.RouteSummary{
+				PointCount:   pointCount,
+				FeatureCount: featureCount,
+				Distance:     distance,
+				ElapsedTime:  int32(endTime.Sub(startTime).Seconds()),
+			})
+		}
+		if err != nil {
+			return err
+		}
+		pointCount++
+		for _, feature := range s.savedFeatures {
+			if proto.Equal(feature.Location, point) {
+				featureCount++
+			}
+		}
+		if lastPoint != nil {
+			distance += calcDistance(lastPoint, point)
+		}
+		lastPoint = point
+	}
+}
+
+func newServer() *sidServer {
+	s := &sidServer{} //routeNotes: make(map[string][]*pb.RouteNote)
+	return s
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *host, *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			*certFile = testdata.Path("server1.pem")
+		}
+		if *keyFile == "" {
+			*keyFile = testdata.Path("server1.key")
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterSidServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
 }
